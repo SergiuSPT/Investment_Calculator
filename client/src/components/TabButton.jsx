e6a65803970379db71109760b1f3aee8a26847ff
SergiuSPT <96991196+SergiuSPT@users.noreply.github.com>
2026-01-02 21:01:17 +0200

    font

--- client/src/components/TabButton.jsx
+++ client/src/components/TabButton.jsx
@@ -1,7 +1,7 @@
 export default function TabButton({isActive, onSelect, children}){
     return(
         <li>
-            <button className={isActive ? "active" : "undefined"} onClick={onSelect}>{children}</button>
+            <button className={isActive ? "active font-roboto" : "undefined"} onClick={onSelect}>{children}</button>
         </li>
     );
 }
